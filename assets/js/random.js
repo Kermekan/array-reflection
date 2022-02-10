068d4e6e6d6a4c0184f41241770176744fe3f042
Kermekan <odeemijr@gmail.com>
2022-02-10 11:53:57 +0000

    no message

--- assets/js/random.js
+++ assets/js/random.js
@@ -6,10 +6,10 @@ const imgButton = document.querySelector("#img-btn");
 
 // Request data from API
 const getImage = async function () {
-    const response = await fetch("https://picsum.photos/v2/list?limit=1000");
+    const response = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await response.json();
     selectRandomImage(images);
-    // console.log(images); // Array of Objects
+    console.log(images); // Array of Picture objects
 };
 
 // Select an image
@@ -17,17 +17,21 @@ const selectRandomImage = function (images) {
     const randomIndex = Math.floor(Math.random() * images.length);
     const randomImage = images[randomIndex];
     displayImage(randomImage);
-    // console.log(randomImage); // Object
+    // console.log(randomImage); // Picture Object
 };
 
 // Show image and author name
+let imageAddress;
 const displayImage = function (randomImage) {
     const author = randomImage.author;
+    
     // pic url
     imageAddress = randomImage.download_url;
     authorSpan.innerText = author;
+    
     img.src = imageAddress;
     imgDiv.classList.remove("hide");
+    console.log(imageAddress);
 };
 
 imgButton.addEventListener("click", () => {
